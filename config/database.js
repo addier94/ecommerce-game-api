a0f70f1f0fa3ecdc4b24836a76bb5f32f1ac48d3
Alfredo Fernandez <alfredo@hotmail.com>
2021-04-24 12:52:32 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'ecommercegame.sfzwk.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'ecommercegame'),
+        username: env('DATABASE_USERNAME', 'testtest'),
+        password: env('DATABASE_PASSWORD', 'testtest'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '29820159-479f-487b-936a-ccaa510cf76a'
+};
